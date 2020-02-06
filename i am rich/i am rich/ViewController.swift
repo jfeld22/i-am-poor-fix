49145d6ac9730600e4479568f85774795e80b56d
jfeld22 <60410743+jfeld22@users.noreply.github.com>
2020-02-06 11:08:13 -0500

    Add files via upload
    homework #1 submitted

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Josh Feld on 1/31/20.
+//  Copyright © 2020 Josh Feld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i am rich/i am richTests/i_am_richTests.swift
@@ -0,0 +1,34 @@
+//
+//  i_am_richTests.swift
+//  i am richTests
+//
+//  Created by Josh Feld on 1/31/20.
+//  Copyright © 2020 Josh Feld. All rights reserved.
+//
+
+import XCTest
+@testable import i_am_rich
+
+class i_am_richTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
